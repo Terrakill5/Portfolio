c17faec06416f21e61e8812074489a46298cb635
JBriceno <josealbertino4@gmail.com>
2024-03-18 21:31:53 -0400

    Actualizado CV 2024

--- src/components/Boton.vue
+++ src/components/Boton.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="flex">
     <a
-      href="https://drive.google.com/file/d/1Kks43BqkvqYlPuwycmDwR6zmWHo1YPFd/view?usp=sharing"
+      href="https://drive.google.com/file/d/1MAtH9G2kqVRW8KtF3Ygb4zwqbQrmcKXc/view?usp=sharing"
       target="_blank"
       class="rounded-30 font-semibold relative border border-solid flex self-start items-center overflow-hidden"
       :class="{'antesLight': light.isLight,'border-secundario1': light.isLight, 'antes': !light.isLight, 'border-secundario': !light.isLight,}"
